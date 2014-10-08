224a8dcd33e9f6ebddc04691a239feba79665c71
Pia d'Iribarne <pia.diribarne@gmail.com>
2014-10-08 16:34:55 +0200

    black jack done

--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/black_jack.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/black_jack.rb
@@ -1,8 +1,12 @@
 def pick_bank_score
+	return rand(16..21)
   # TODO: Use Random to get a new random score
 end
 
-def pick_card
+def pick_player_card
+	return rand(1..11)
   # TODO: Use Random to get a new random card
 end
+
+
 
--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
@@ -1,15 +1,35 @@
 require_relative 'black_jack'
 
 def state_of_the_game(player_score, bank_score)
+	return "Your score is #{player_score}, bank is #{bank_score}"
   # TODO: return (not print!) a message containing the player's score and bank's score
 end
 
 def end_game_message(player_score, bank_score)
+    if player_score > 21 
+    	return "Lose"
+    elsif player_score == 21
+    	return "Black Jack"
+    elsif player_score >= bank_score
+    	return "win"
+    else 
+    	return "lose"
+    end  	
   # TODO: return (not print!) a message telling if the user won or lost.
 end
 
 
 def play_game
-  # TODO: make the user play from terminal in a while loop that will stop
-  #       when the user will not be asking for  a new card
-end
+	puts "Card? type 'y' or 'yes' for a new card"
+	player_answer = gets.chomp
+
+	player_score = 0
+	bank_score = pick_bank_score
+	while player_answer == "y" || player_answer == "yes"
+		player_score = pick_player_card + player_score 
+		puts state_of_the_game(player_score, bank_score)
+		puts "Card? type 'y' or 'yes' for a new card"
+		player_answer = gets.chomp
+	end
+	puts end_game_message(player_score, bank_score)
+end 
