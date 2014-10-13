7245bf5603428d56217fa694231c2692332c8109
Pia d'Iribarne <pia.diribarne@gmail.com>
2014-10-13 13:04:47 +0200

    phone number

--- 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
+++ 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
@@ -1,3 +1,6 @@
 def french_phone_number?(phone_number)
   # TODO: true or false?
+  puts "Enter your phone number"
+  phone_number.gets.chomp
+  phone_number.match(/0|\+33\s?)[6|7](\s?\d{2}){4}/)
 end
