94edc4e90dbd9f170c5dc51335eaf8986f37482c
Pia d'Iribarne <pia.diribarne@gmail.com>
2014-10-13 09:14:36 +0200

    mcdonald

--- 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
+++ 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
@@ -1,7 +1,39 @@
+DISHES = {
+  "Cheese Burger" => 290,
+  "Big Mac" => 300,
+  "Mc Bacon" => 400,
+  "Royal Cheese" => 130,
+  "French fries" => 130,
+  "Potatoes" => 130,
+  "Coca" => 160,
+  "Sprite" => 170
+}
+
+MENUS = {
+  "Happy Meal"=> ["Cheese Burger", "French fries", "Coca"],
+  "Best Of Big Mac" => ["Big Mac", "French fries", "Coca"],
+  "Best Of Royal Cheese" => ["Royal Cheese", "Potatoes", "Sprite"]
+}
+
 def poor_calories_counter(burger, side, beverage)
-  #TODO: return number of calories for this mcDonald order
+  DISHES[burger] + DISHES[side] + DISHES[beverage]
 end
 
 def calories_counter(*orders)
-  #TODO: return number of calories for a less constrained order
+  orders.reduce(0) do |calories, order|
+    if DISHES[order]  # The order is a simple dish
+      calories += DISHES[order]
+    else              # The order is a full menu
+      calories += DISHES.values_at(*MENUS[order]).reduce(:+)
+    end
+  end
 end
+
+
+
+
+
+
+
+
+
