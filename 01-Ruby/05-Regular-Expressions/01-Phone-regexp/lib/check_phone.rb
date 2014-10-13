22846118d249a0cfe80896c27af146d5b6ecd2fd
Pia d'Iribarne <pia.diribarne@gmail.com>
2014-10-13 15:17:50 +0200

    better syntax with ternary operator

--- 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
+++ 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
@@ -1,9 +1,4 @@
 def french_phone_number?(phone_number)
   # TODO: true or false?
-  if phone_number =~ /(0|\+33)[\s-]?[6]([\s-]?\d{2}){4}/
-  	return true
-  else
-  	return false
-  end
-end
-
+	(phone_number =~ /(0|\+33)[\s-]?[6]([\s-]?\d{2}){4}/) ? true : false
+end	
