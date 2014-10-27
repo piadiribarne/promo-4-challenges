c8a114ecf71cabffdc772224bfe54b2251efee80
Pia d'Iribarne <pia.diribarne@gmail.com>
2014-10-27 16:30:58 +0100

    exo2

--- 03-AR-Database/03-ActiveRecord-Basics/02-Models/app/controllers/posts_controller.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/02-Models/app/controllers/posts_controller.rb
@@ -9,25 +9,44 @@ class PostsController
     # DO NOT WRITE SQL QUERIES
     # TODO: gather all posts from the database
     # TODO: give them to the view to be printed
+    all_posts = Post.all
+    @view.view_all_posts(all_posts)
+
   end
 
   def create
     # DO NOT WRITE SQL QUERIES
     # TODO: create a post
+    post_data = @view.create_post
+    Post.create(post_data)
   end
 
   def update
     # DO NOT WRITE SQL QUERIES
     # TODO: update a post
+    id = @view.ask_for_id
+    updated_data = @view.ask_updated_data
+    post = Post.find(id)
+    post.update_attributes(updated_data)
   end
 
   def destroy
     # DO NOT WRITE SQL QUERIES
     # TODO: destroy a post
+    id = @view.ask_for_id
+    post = Post.find(id)
+    post.delete
   end
 
   def upvote
     # DO NOT WRITE SQL QUERIES
     # TODO: increment the `votes` column for a post
+    id = @view.ask_for_id
+    post = Post.find(id)
+    if post.votes == nil
+      post.votes = 0
+    end
+    post.votes = post.votes + 1
+    post.save
   end
 end
--- 03-AR-Database/03-ActiveRecord-Basics/02-Models/app/views/post_view.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/02-Models/app/views/post_view.rb
@@ -1,3 +1,36 @@
 class PostView
   # TODO: implement some methods here when controller needs to `puts` or `gets`
+  def display_post(post)
+    puts "Post title: #{post.name} - Url : #{post.url} - created at: #{post.created_at} - votes: #{post.votes}"
+  end
+
+  def create_post
+    puts "Title?"
+    name = gets.chomp
+    puts "url?"
+    url = gets.chomp
+    return {name: name, url: url}
+  end
+
+
+  def view_all_posts(posts)
+    posts.each do |post|
+      display_post(post)
+    end
+  end
+
+  def ask_for_id
+    puts "ID?"
+    return id = gets.chomp.to_i
+  end
+
+  def ask_updated_data
+    puts "Updated Title?"
+    name = gets.chomp
+    puts "Udated url?"
+    url = gets.chomp
+    return {name: name, url: url}
+  end
+
 end
+
