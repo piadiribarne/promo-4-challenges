4cef80d9a17e76bdb330b6cd8828fd3993d1581e
Pia d'Iribarne <pia.diribarne@gmail.com>
2014-10-27 17:10:14 +0100

    lol

--- 03-AR-Database/03-ActiveRecord-Basics/03-Seed-Faker/db/seeds.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/03-Seed-Faker/db/seeds.rb
@@ -1,3 +1,10 @@
 require "faker"
 
 # TODO: Write a seed to insert 100 posts in the database
+100.times do
+  post = Post.new({
+    name: Faker::Name.name,
+    url: "#{Faker::Internet.url}",
+  })
+  post.save
+end
