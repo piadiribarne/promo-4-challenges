3c58933cb8a6847c5d7fa178007da2ad19814086
Pia d'Iribarne <pia.diribarne@gmail.com>
2014-10-27 14:22:55 +0100

    exo

--- 03-AR-Database/03-ActiveRecord-Basics/01-Migrations/db/migrate/20141025152200_create_posts.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/01-Migrations/db/migrate/20141025152200_create_posts.rb
@@ -1,5 +1,10 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     # TODO: your code here to create the posts table
+    create_table :posts do |t|
+      t.string :name
+      t.string :url
+      t.timestamps
+    end
   end
 end
