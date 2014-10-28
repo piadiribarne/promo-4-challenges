dc958613661642d84ae41ab5d803241b3898fa2f
Pia d'Iribarne <pia.diribarne@gmail.com>
2014-10-28 12:44:09 +0100

    done

--- 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/seeds.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/seeds.rb
@@ -1,1 +1,16 @@
-# TODO: Write a seed
+require "faker"
+
+5.times do
+  user = User.new({
+    username: Faker::Name.name,
+    email: Faker::Internet.email,
+  })
+  user.save
+(5..10).to_a.sample.times do
+user.posts.create({
+  name: Faker::Name.name,
+  url: "#{Faker::Internet.url}",
+  votes: (0..10).to_a.sample
+  })
+end
+end
