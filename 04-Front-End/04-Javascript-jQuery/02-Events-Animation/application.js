a2835b3c1efc10783690b65ec27db57d0aa4d014
Pia d'Iribarne <pia.diribarne@gmail.com>
2014-11-04 10:48:05 +0100

    bar

--- 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
+++ 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
@@ -1,5 +1,25 @@
-/*
-  *
-  * TODO: add your code here!
-  *
-*/
+$(function(){
+
+
+
+
+ $('#message').hide();
+
+ $('#message').delay(1000).slideDown([2000]);
+
+ $('#message').on('click', function(the_event) {
+   $(this).slideUp()});
+
+ $('#message-wrapper').mouseenter( function() {
+   $('#message').slideDown();
+ }).mouseleave(function(){
+   $('#message').slideUp();
+ });
+
+});
+
+
+
+
+
+
